ada31e677989e211a4e1c101ad03e523ab2ba598
Doria <dorialassalle@yahoo.com>
2021-06-21 12:59:57 -0400

    mejora funciones delete

--- books_authors_app/views.py
+++ books_authors_app/views.py
@@ -12,11 +12,11 @@ def index(request):
 
 
 def showBook(request, bookId):
-    bookInfo=Book.objects.get(id=bookId)#objeto
-    title=bookInfo.title
+    bookInfo=Book.objects.get(id=bookId)#traigo el objeto por el id que llega en la url
+    title=bookInfo.title #extaigo del objeto lo que voy a utillizr y lo guardo en la variable
     desc=bookInfo.desc
-    bookAuthors=bookInfo.authors.all()#consulto por el objeto
-    allAuthors=Author.objects.all()
+    bookAuthors=bookInfo.authors.all()#traigo todos los autores de este libro(objeto)
+    allAuthors=Author.objects.all()#traigo todos los autores (para el menu desplegable)
 
     context={
         "bookId": bookId,
@@ -28,14 +28,14 @@ def showBook(request, bookId):
 
     if request.method=='POST':
         chosenAuthor=request.POST["chooseAuthor"]
-        bookInfo.authors.add(chosenAuthor)    
-    return render(request, "books.html", context)
+        bookInfo.authors.add(chosenAuthor)    #agrego un autor a este libro
+    return render(request, "books.html", context)  
 
 
 def addNewBook(request):
-    newTitle=request.POST["title"]
+    newTitle=request.POST["title"] #guardo la info que viene del formulario
     newDesc=request.POST["desc"]
-    Book.objects.create(title=newTitle, desc=newDesc)    
+    Book.objects.create(title=newTitle, desc=newDesc)    #creo el nuevo objeto conla info recibida
     return redirect("/")
 
 
@@ -52,16 +52,16 @@ def addNewAuthor(request):
     newName=request.POST["firstName"]
     newLastName=request.POST["lastName"]
     newNote=request.POST["newNote"]
-    Author.objects.create(first_name=newName, last_name=newLastName, notes=newNote)
+    Author.objects.create(first_name=newName, last_name=newLastName, notes=newNote)# creo un nuevoonjeto conla info recibida del form
     return redirect("/authors")
 
 def showAuthor(request, authorId):
-    thisAuthor=Author.objects.get(id=authorId)
+    thisAuthor=Author.objects.get(id=authorId) #traigo el objeto por el id recibido en la url
     name=thisAuthor.first_name
     lastName=thisAuthor.last_name
     notes=thisAuthor.notes
-    authorBooks=thisAuthor.books.all()
-    allBooks=Book.objects.all()
+    authorBooks=thisAuthor.books.all() #traigo los libros de este autor
+    allBooks=Book.objects.all()#traigo todos los autores (para el menu desplegable)
 
     context={
         "authorId":authorId,
@@ -74,26 +74,22 @@ def showAuthor(request, authorId):
 
     if request.method=='POST':
         chosenBook=request.POST["chooseBook"]
-        thisAuthor.books.add(chosenBook)
+        thisAuthor.books.add(chosenBook) #agrego un libro a este autor
     return render(request,"oneauthor.html", context)
 
 
 def deleteAuthor(request, authorId):
     authorToDelete=Author.objects.get(id=authorId)
-    for book in authorToDelete.books.all():
-        bookId=book.id
+    currentBook=request.POST["bookId"]  #libro que se está mostrando      
     authorToDelete.delete() #elimino el autor del libro pero no el libro
-    return redirect(f"/books/{bookId}")
+    return redirect(f"/books/{currentBook}") #reddirecciono al mismo libro donde estaba
 
 
 def deleteBook(request, bookId):
     bookToDelete=Book.objects.get(id=bookId)#recupero el libro a eliminar
-    for author in bookToDelete.authors.all(): #recorro su lista de autores  
-        authorId=author.id                      
-        #author.books.remove(bookToDelete) #elimina el libro para cada autor?...
-        print(authorId)  
+    currentAuthor=request.POST["authorId"]#guardo el autor que se esta mostrando para recargar el mismp  
     bookToDelete.delete()  
-    return redirect(f"/author/{authorId}")
+    return redirect(f"/author/{currentAuthor}")
 
    
     
